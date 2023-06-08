a2b789f3896064ad43a35d6337c13ce31b09b0fa
NinoTraverso <njt98git@proton.me>
2023-06-08 14:52:17 +0000

    first commit

---
+++ Compito/index.js
@@ -0,0 +1,27 @@
+/*questa funzione ci permette di creare 77 numberi casuali dentro un array*/
+
+const numbers = [];
+
+const randomNumberGenerator = function () {
+  const number = [];
+  for (let i = 0; i < 77; i++) {
+    const randomNumber = Math.floor(Math.random() * 77) + 1;
+    numbers.push(randomNumber);
+    number.push(randomNumber);
+  }
+  return number;
+};
+
+function displayNumbersGenerated(click) {
+  click.preventDefault();
+  const randomNumbers = randomNumberGenerator();
+  console.log(randomNumbers);
+}
+
+console.log("Your numbers are:", numbers);
+
+/*qui mettiamo il codice che ci permette di creare una nuova lista random di numri*/
+const clickToGenerateNumber = document.getElementById("numberGeneratorButton");
+clickToGenerateNumber.addEventListener("click", displayNumbersGenerated);
+
+/*qui creiamo un modo per mettere i numeri generati dentro ogni td del table(id="table") del div(id="bingoBoard")*/
